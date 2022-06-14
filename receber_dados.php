100403af879654454fceea5a418da661d498f31b
IzzyViegas <Israelineprofissional@gmail.com>
2022-06-14 18:18:42 -0300

    main

--- receber_dados.php
+++ receber_dados.php
@@ -1,17 +1,14 @@
 <!DOCTYPE html>
 <html>
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Jubinha_Black 2022</title>
 </head>
-
 <body>
   <?php
-  include_once("topo.php");
+   include_once("topo.php");
   ?>
-
   <div id="corpo">
     <main>
       <h2> Dados Recebidos</h2>
@@ -21,13 +18,11 @@
         $cidade = $_POST['cidade'];
         $uf = $_POST['estado'];
         $msg = $_POST['mensagem'];
-
         echo "Nome:" . $nome . "<br>";
         echo "E-mail:" . $email . "<br>";
         echo "cidade:" . $cidade . "<br>";
         echo "Estado:" . $uf . "<br>";
         echo "Mensagem:" . $msg . "<br>";
-    
         $contato = array("Nome" => $nome, "Email" => $email, "UF" => $uf, "cidade" => $cidade, "Mensagem" => $msg);
         echo "<hr> Vetor criado: <br>";
         print_r($contato);
@@ -35,7 +30,6 @@
           $string = file_get_contents("dados.json");
           $json = json_decode($string, true);
         }
-
         $json["contatos"][] = $contato;
          $fp = fopen('dados.json', 'w');
          if ($fp == false) {
@@ -43,7 +37,6 @@
           }
         fwrite($fp, json_encode($json));
         fclose($fp); 
-
       ?>
     </main>
   </div>
